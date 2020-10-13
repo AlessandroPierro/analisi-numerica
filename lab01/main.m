536d4995e10d8824a932b6786c994493a0e7cd1b
Alessandro Pierro <mail.alessandropierro@gmail.com>
2020-10-13 22:05:32 +0200

    Added basic functionalities to main M-script in lab01

--- lab01/main.m
+++ lab01/main.m
@@ -1,10 +1,29 @@
 n = 9;
 m = 100;
 
-f = @(x, y) 1 / (1 + 25 * (x.^2 + y.^2));
+f = @(x, y) 1 ./ (1 + 25 * ((x.^2) + (y.^2)));
 
 z = linspace(-1, 1, m);
 x = linspace(-1, 1, n);
 
 L = Lagrange(x, z);
+F = zeros(n);
+P = zeros(m);
 
+for i = 1:9
+    
+    F(i, :) = f(x(i), x);
+    
+end
+
+for h = 1:m
+   
+    for k = h:m
+    
+        P(h, k) = (F * L(:, k))' * L(:, h);
+    
+    end
+    
+end
+
+P = (P + P') - eye(m).*diag(P);
