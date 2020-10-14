cd8c0bab39e39122a51132675e911a616fe9c350
Alessandro Pierro <mail.alessandropierro@gmail.com>
2020-10-14 10:26:39 +0200

    Code refactoring: created Interpole M-function in lab01

---
+++ lab01/Interpole.m
@@ -0,0 +1,25 @@
+function [P] = Interpole(f, x, z)
+    
+    n = length(x);
+    m = length(z);
+    
+    L = Lagrange(x, z);
+    P = zeros(m);
+    
+    [X, Y] = meshgrid(x, x);
+    F = f(X, Y);
+    
+    for h = 1:m
+   
+        for k = h:m
+    
+            P(h, k) = (F * L(:, k))' * L(:, h);
+        
+        end
+    
+    end
+    
+    P = (P + P') - eye(m).*diag(P);
+    
+end
+
--- lab01/main.m
+++ lab01/main.m
@@ -6,33 +6,7 @@ theta = @(t, n) cos((2*t - 1) * pi / (2*n));
 
 z = linspace(-1, 1, m);
 x = linspace(-1, 1, n);
-x_1 = theta(1:n, n);
+x_chebychev = theta(1:n, n);
 
-L = Lagrange(x, z);
-F = zeros(n);
-P = zeros(m);
-
-L_1 = Lagrange(x_1, z);
-F_1 = zeros(n);
-P_1 = zeros(m);
-
-for i = 1:9
-    
-    F(i, :) = f(x(i), x);
-    F_1(i, :) = f(x_1(i), x_1);
-    
-end
-
-for h = 1:m
-   
-    for k = h:m
-    
-        P(h, k) = (F * L(:, k))' * L(:, h);
-        P_1(h, k) = (F_1 * L_1(:, k))' * L_1(:, h);
-    
-    end
-    
-end
-
-P = (P + P') - eye(m).*diag(P);
-P_1 = (P_1 + P_1') - eye(m).*diag(P_1);
+P = Interpole(f, x, z);
+P_chebychev = Interpole(f, x_chebychev, z);
