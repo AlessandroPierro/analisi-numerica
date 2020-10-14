719630b047832048215f1a2def128c9ed2966403
Alessandro Pierro <mail.alessandropierro@gmail.com>
2020-10-14 10:27:38 +0200

    Added plotting to main.m in lab01

--- lab01/main.m
+++ lab01/main.m
@@ -10,3 +10,23 @@ x_chebychev = theta(1:n, n);
 
 P = Interpole(f, x, z);
 P_chebychev = Interpole(f, x_chebychev, z);
+
+[X, Y] = meshgrid(z, z);
+F = f(X, Y);
+
+% Plotting
+figure;
+hold on;
+subplot(2, 2, 1);
+mesh(X, Y, F);
+title("2D Runge Function");
+subplot(2, 2, 2);
+mesh(P);
+title("Polynomial Interpolation - Equally spaced nodes");
+subplot(2, 2, 3);
+mesh(X, Y, F);
+title("2D Runge Function");
+subplot(2, 2, 4);
+mesh(P_chebychev);
+title("Polynomial Interpolation - Chebychev nodes");
+hold off;
